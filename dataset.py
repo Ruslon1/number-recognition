774346df84408d120ae99d6f791fa9a3031a1152
Ruslon1 <84863005+Ruslon1@users.noreply.github.com>
2025-02-16 14:52:27 +0500

    data loading

--- dataset.py
+++ dataset.py
@@ -1,3 +1,11 @@
 import torch
 import torchvision
 import torchvision.transforms as transfroms
+
+transfrom = transfroms.Compose(transfroms.ToTensor, transfroms.Normalize((0.5,), (0.5,)))
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transfrom)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transfrom)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
