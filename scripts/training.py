80c286131309b8f1df0bdbf72dca0d66a8f13834
Ruslon1 <84863005+Ruslon1@users.noreply.github.com>
2025-02-17 18:02:29 +0500

    Checkpoint system

--- scripts/training.py
+++ scripts/training.py
@@ -3,13 +3,26 @@ import torch.nn as nn
 import torch.optim as optim
 import cnn
 import dataset
+import os
 
 model = cnn.CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 device = torch.device('cpu')
 
-for epoch in range(1):
+checkpoint_path = '../checkpoint.pth'
+
+if os.path.exists(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    start_epoch = checkpoint['epoch'] + 1
+    print(f"Resuming training from epoch {start_epoch}.")
+else:
+    start_epoch = 0
+    print("Starting from scratch")
+
+for epoch in range(start_epoch, start_epoch + 10):
     model.train()
     running_loss = 0.0
     for images, lables in dataset.train_loader:
@@ -27,4 +40,9 @@ for epoch in range(1):
 
     print(f"Epoch {epoch+1}, Loss: {running_loss/len(dataset.train_loader):.4f}")
 
-torch.save(model.state_dict(), '../model.pth')
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch,
+        'loss': running_loss
+    }, checkpoint_path)
